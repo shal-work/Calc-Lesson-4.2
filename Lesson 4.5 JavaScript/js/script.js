e729d13e95d8709708ddfb914a466688e6168727
shal-work <shal.work@mail.ru>
2020-11-30 13:52:02 +0300

    ”Second commit added a lesson 4.5 Promise”

---
+++ Lesson 4.5 JavaScript/js/script.js
@@ -0,0 +1,58 @@
+//Домашняя задачи 4.5 (Задание 2) реализовать в Promise
+let inputRub = document.getElementById('rub'),
+    inputUsd = document.getElementById('usd');
+
+inputRub.addEventListener('input', () => {
+    let request = new XMLHttpRequest();
+
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+
+    request.addEventListener('readystatechange', function () {
+
+        const promise = new Promise((resolve, reject) => {
+            if (request.readyState === 4 && request.status == 200) {
+                resolve();
+            } else {
+                reject();
+            }
+        });
+
+        promise
+            .then(() => {
+                let data = JSON.parse(request.response);
+                inputUsd.value = inputRub.value / data.usd;
+            })
+            .catch(() => {
+                inputUsd.value = "Что-то пошло не так!";
+            });
+
+    });
+    
+});
+
+
+
+//код без Promise
+// let inputRub = document.getElementById('rub'),
+//     inputUsd = document.getElementById('usd');
+
+// inputRub.addEventListener('input', () => {
+//     let request = new XMLHttpRequest();
+
+//     request.open('GET', 'js/current.json');
+//     request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+//     request.send();
+
+//     request.addEventListener('readystatechange', function() {
+//         if (request.readyState === 4 && request.status == 200) {
+//             let data = JSON.parse(request.response);
+
+//             inputUsd.value = inputRub.value / data.usd;
+//         } else {
+//             inputUsd.value = "Что-то пошло не так!";
+//         }
+//     });
+
+// });
